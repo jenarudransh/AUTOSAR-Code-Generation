0a6a2a0c6e4ed62a3d6082d1e83d3aee7b40f9cd
Rudransh Jena <jenarudransh@gmail.com>
2025-10-20 14:37:17 +0530

    C Header Source file

---
+++ AEB_CodeGeneration/AEB_CodeGeneration_autosar_rtw/AEB_CodeGeneration_private.h
@@ -0,0 +1,30 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: AEB_CodeGeneration_private.h
+ *
+ * Code generated for Simulink model 'AEB_CodeGeneration'.
+ *
+ * Model version                  : 1.2
+ * Simulink Coder version         : 25.1 (R2025a) 21-Nov-2024
+ * C/C++ source code generated on : Mon Oct 20 14:22:02 2025
+ *
+ * Target selection: autosar.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef AEB_CodeGeneration_private_h_
+#define AEB_CodeGeneration_private_h_
+#include "Platform_Types.h"
+#include "AEB_CodeGeneration_types.h"
+#endif                                 /* AEB_CodeGeneration_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
